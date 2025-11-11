56cbbd3f8ffa2e95ec4ba3b6091c4a0316a40dce
dev000012 <codingninjadevs@gmail.com>
2025-11-11 18:40:44 +0530

    Initial commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
@@ -7,19 +7,24 @@ import Contact from './pages/Contact'
 import Home from './pages/Home'
 import Login from './pages/Login'
 
-const App = () => (
-  <div className="flex min-h-screen flex-col bg-slate-100 text-slate-900">
-    <Navbar />
-    <main className="flex-1">
-      <Routes>
-        <Route element={<Home />} path="/" />
-        <Route element={<About />} path="/about" />
-        <Route element={<Contact />} path="/contact" />
-        <Route element={<Login />} path="/login" />
-      </Routes>
-    </main>
-    <Footer />
-  </div>
-)
+const App = () => {
+  const location = useLocation()
+  const isLoginPage = location.pathname === '/login'
+
+  return (
+    <div className="flex min-h-screen flex-col bg-slate-100 text-slate-900">
+      {!isLoginPage && <Navbar />}
+      <main className="flex-1">
+        <Routes>
+          <Route element={<Home />} path="/" />
+          <Route element={<About />} path="/about" />
+          <Route element={<Contact />} path="/contact" />
+          <Route element={<Login />} path="/login" />
+        </Routes>
+      </main>
+      {!isLoginPage && <Footer />}
+    </div>
+  )
+}
 
 export default App
